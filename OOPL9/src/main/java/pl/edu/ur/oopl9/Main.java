ca570ade3a518567bbb01a103c033eb75d5c48e4
Marcin <mchyla@ur.edu.pl>
2018-05-07 07:03:07 +0200

    init

---
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.ur.oopl9;
+
+/**
+ *
+ * @author maarchyl
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
